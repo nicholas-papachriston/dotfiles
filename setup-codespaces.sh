24059d03e23e25694c0cae8d348fd2ca4ca409a6
Nicholas Romero <nicholas.romero@unsupervised.com>
2025-01-30 10:13:36 -0600

    feat: switch installing packages via brew on both linux and mac

--- setup-codespaces.sh
+++ setup-codespaces.sh
@@ -11,23 +11,8 @@ cd $HOME
 # Make passwordless sudo work
 export SUDO_ASKPASS=/bin/true
 
-# Apt shouldn't ask any questions
-export DEBIAN_FRONTEND=noninteractive
-
 # Change shell to zsh
 sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
-
-# Install useful packages
-sudo -E apt-get update
-sudo -E apt-get install -y tree libfuse2 stow
-
-# Install neovim
-# Ubuntu is the worst
-curl -L -o nvim.appimage https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-sudo install -vD -m 755 nvim.appimage /usr/local/bin/nvim
-rm nvim.appimage
-
-cd dotfiles
 
 stow nvim ruby zsh git
 
